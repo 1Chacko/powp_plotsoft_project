7274a229f13d450efcfd4ed95225712e71be7cee
1Chacko <kuba.olczak.jo@gmail.com>
2017-05-18 00:45:02 +0200

    Dodany mirror dla iis

--- src/test/java/edu/iis/powp/events/SelectMirrorCommandOptionListener.java
+++ src/test/java/edu/iis/powp/events/SelectMirrorCommandOptionListener.java
@@ -6,7 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import edu.iis.powp.appext.FeaturesManager;
+import edu.iis.powp.command.DrawToCommand;
 import edu.iis.powp.command.IPlotterCommand;
+import edu.iis.powp.command.SetPositionCommand;
 import edu.iis.powp.command.manager.PlotterCommandManager;
 
 public class SelectMirrorCommandOptionListener implements ActionListener {
@@ -15,7 +17,20 @@ public class SelectMirrorCommandOptionListener implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		List<IPlotterCommand> commands = new ArrayList<IPlotterCommand>(); 
 		
-		//wypelnic poleceniami
+		commands.add(new SetPositionCommand(70, -50));
+		commands.add(new DrawToCommand(70, -50));
+		commands.add(new SetPositionCommand(70, -40));
+		commands.add(new DrawToCommand(70, 50));
+		commands.add(new SetPositionCommand(50, -50));
+		commands.add(new DrawToCommand(50, -50));
+		commands.add(new SetPositionCommand(50, -40));
+		commands.add(new DrawToCommand(50, 50));
+		commands.add(new SetPositionCommand(-20, -50));
+		commands.add(new DrawToCommand(30, -50));
+		commands.add(new DrawToCommand(30, 0));
+		commands.add(new DrawToCommand(-20, 0));
+		commands.add(new DrawToCommand(-20, 50));
+		commands.add(new DrawToCommand(30, 50));
 		
 		PlotterCommandManager manager = FeaturesManager.getPlotterCommandManager();
 		manager.setCurrentCommand(commands, "MirrorCommand");
