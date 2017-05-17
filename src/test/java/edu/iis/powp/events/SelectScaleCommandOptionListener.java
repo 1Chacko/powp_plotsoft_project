bb0ba11dabf8587922920a4b71a4fb978ee40939
Blazej Lewandowski <ledek89@gmail.com>
2017-05-18 01:16:02 +0200

    Dodana funkcjolanosc skalowania

--- src/test/java/edu/iis/powp/events/SelectScaleCommandOptionListener.java
+++ src/test/java/edu/iis/powp/events/SelectScaleCommandOptionListener.java
@@ -6,7 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import edu.iis.powp.appext.FeaturesManager;
+import edu.iis.powp.command.DrawToCommand;
 import edu.iis.powp.command.IPlotterCommand;
+import edu.iis.powp.command.SetPositionCommand;
 import edu.iis.powp.command.manager.PlotterCommandManager;
 
 public class SelectScaleCommandOptionListener implements ActionListener {
@@ -14,12 +16,23 @@ public class SelectScaleCommandOptionListener implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		List<IPlotterCommand> commands = new ArrayList<IPlotterCommand>(); 
+		commands.add(new SetPositionCommand(-40, -100));
+		commands.add(new DrawToCommand(-40, -100));
+		commands.add(new SetPositionCommand(-40, -80));
+		commands.add(new DrawToCommand(-40, 100));
+		commands.add(new SetPositionCommand(0, -100));
+		commands.add(new DrawToCommand(0, -100));
+		commands.add(new SetPositionCommand(0, -80));
+		commands.add(new DrawToCommand(0, 100));
+		commands.add(new SetPositionCommand(140, -100));
+		commands.add(new DrawToCommand(40, -100));
+		commands.add(new DrawToCommand(40, 0));
+		commands.add(new DrawToCommand(140, 0));
+		commands.add(new DrawToCommand(140, 100));
+		commands.add(new DrawToCommand(40, 100));
 		
-		//wypelnic poleceniami
-		
-		PlotterCommandManager manager = FeaturesManager.getPlotterCommandManager();
-		manager.setCurrentCommand(commands, "ScaleCommand");
-
+	    PlotterCommandManager manager = FeaturesManager.getPlotterCommandManager();
+	    manager.setCurrentCommand(commands, "ScaleCommans");
 	}
 
 }
