4dd0213fcb030196a048af197d4904b712eb4968
Michał Bodzioch <michbodz@gmail.com>
2017-05-25 00:32:09 +0200

    Przesuwanie(itp.) o podany x i y. Refaktoryzacja kodu wymagana.

--- src/main/java/edu/iis/powp/command/FactoryCommand.java
+++ src/main/java/edu/iis/powp/command/FactoryCommand.java
@@ -8,20 +8,19 @@ import edu.iis.powp.command.manager.PlotterCommandManager;
 
 public class FactoryCommand {	
 		 	
- 	public static ComplexCommand createRectangle(int x, int y, int height, int width ) {
+ 	public static ComplexCommand createRectangle(int x, int y, int height, int width, int xMove, int yMove ) {
  		//zapytanie o wartosci i kierunki przesuniec, odbic itd.
  		PlotterCommandManager pcm = FeaturesManager.getPlotterCommandManager(); 		
  		String command = pcm.getCurrentCommandString();
  		List<PlotterCommand> commandArrayList = new ArrayList<PlotterCommand>();
  		
  		if(command.equals("MoveCommand")){ 
- 			//tu powinno byc zapytanie o ile przesunac, zeby mozna bylo wpisac wartosci w oznaczonym przez "move" miejscu
- 			int move = -75;
- 			commandArrayList.add(new CommandSetPosition(x + move, y + move));
- 	 		commandArrayList.add(new CommandDrawLineToPosition(x + move + width, y + move));
- 	 		commandArrayList.add(new CommandDrawLineToPosition(x + move + width, y + move + height));
- 	 		commandArrayList.add(new CommandDrawLineToPosition(x + move, y + move + height));
- 	  		commandArrayList.add(new CommandDrawLineToPosition(x + move, y + move));
+ 			//tu powinno byc zapytanie o ile przesunac, zeby mozna bylo wpisac wartosci w oznaczonym przez "move" miejscu 	
+ 			commandArrayList.add(new CommandSetPosition(x + xMove, y + yMove));
+ 	 		commandArrayList.add(new CommandDrawLineToPosition(x + xMove + width, y + yMove));
+ 	 		commandArrayList.add(new CommandDrawLineToPosition(x + xMove + width, y + yMove + height));
+ 	 		commandArrayList.add(new CommandDrawLineToPosition(x + xMove, y + yMove + height));
+ 	  		commandArrayList.add(new CommandDrawLineToPosition(x + xMove, y + yMove));
  		} else if(command.equals("ScaleCommand")) {
  			//funkcjonalnosc
  		} else if(command.equals("MirrorCommand")) {
@@ -40,7 +39,7 @@ public class FactoryCommand {
    		return new ComplexCommand(commandArrayList);
   	}
  	
- 	public static ComplexCommand createRightTrapezoid(int x, int y, int height, int width ) {
+ 	public static ComplexCommand createRightTrapezoid(int x, int y, int height, int width, int xMove, int yMove ) {
  		
  		PlotterCommandManager pcm = FeaturesManager.getPlotterCommandManager(); 		
  		String command = pcm.getCurrentCommandString();
@@ -49,11 +48,11 @@ public class FactoryCommand {
  		if(command.equals("MoveCommand")){ 
  			//tu powinno byc zapytanie o ile przesunac, zeby mozna bylo wpisac wartosci w oznaczonym przez "move" miejscu
  			int move = -75;
- 			commandArrayList.add(new CommandSetPosition(x + move, y + move));
- 	 		commandArrayList.add(new CommandDrawLineToPosition(x + move + width, y + move));
- 	 		commandArrayList.add(new CommandDrawLineToPosition(x + move + width - (width / 3), y + move + height));		
- 	 		commandArrayList.add(new CommandDrawLineToPosition(x + move, y + move + height));		
- 	 		commandArrayList.add(new CommandDrawLineToPosition(x + move, y + move));
+ 			commandArrayList.add(new CommandSetPosition(x + xMove, y + yMove));
+ 	 		commandArrayList.add(new CommandDrawLineToPosition(x + xMove + width, y + yMove));
+ 	 		commandArrayList.add(new CommandDrawLineToPosition(x + xMove + width - (width / 3), y + yMove + height));		
+ 	 		commandArrayList.add(new CommandDrawLineToPosition(x + xMove, y + yMove + height));		
+ 	 		commandArrayList.add(new CommandDrawLineToPosition(x + xMove, y + yMove));
  		} else if(command.equals("ScaleCommand")) {
  			//funkcjonalnosc
  		} else if(command.equals("MirrorCommand")) {
--- src/test/java/edu/iis/powp/events/SelectTestFiguresOptionListener.java
+++ src/test/java/edu/iis/powp/events/SelectTestFiguresOptionListener.java
@@ -3,26 +3,116 @@ package edu.iis.powp.events;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 import edu.iis.powp.appext.FeaturesManager;
 import edu.iis.powp.command.FactoryCommand;
+import edu.iis.powp.command.manager.PlotterCommandManager;
 
 public class SelectTestFiguresOptionListener implements ActionListener{
 	
-	String checkWhichTest;
+	String checkWhichTest;	
 	
 	public SelectTestFiguresOptionListener(String string) {
  		this.checkWhichTest = string;
  	}
 	
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		if(checkWhichTest.equals("Rectangle")) {
-			FactoryCommand.createRectangle(-50, -50, 100, 200).execute(FeaturesManager.getDriverManager().getCurrentPlotter());
- 		} else if(checkWhichTest.equals("RightTrapezoid")) {
- 			FactoryCommand.createRightTrapezoid(-50, -50, 100, 200).execute(FeaturesManager.getDriverManager().getCurrentPlotter());
+	public void actionPerformed(ActionEvent arg0) {	
+		PlotterCommandManager pcm = FeaturesManager.getPlotterCommandManager(); 		
+ 		String command = pcm.getCurrentCommandString();
+ 		
+ 		if(!command.equals("No command loaded")) {
+ 			if(checkWhichTest.equals("Rectangle")) {			
+			    JDialog frame = new JDialog();
+			    frame.setLayout(null);
+			    frame.setSize(230, 170);
+			    frame.setLocation(300, 250);
+			    JTextField x = new JTextField();
+			    JLabel xLabel = new JLabel("Przesunięcie x");
+			    JTextField y = new JTextField(); 	   
+			    JLabel yLabel = new JLabel("Przesunięcie y");
+			    JButton okButton = new JButton("OK");
+			    
+			    okButton.addActionListener (new ActionListener () {
+			        public void actionPerformed(ActionEvent e) {			        			        			        	
+			        	frame.dispose();
+			        	FactoryCommand.createRectangle(-50, -50, 100, 200, 	Integer.parseInt(x.getText()), Integer.parseInt(y.getText())).execute(FeaturesManager.getDriverManager().getCurrentPlotter());
+			        }
+			    });	 	    
+			    
+			    x.setSize(50, 25);
+			    y.setSize(50, 25);
+			    xLabel.setSize(100, 25);
+			    yLabel.setSize(100, 25);
+			
+			    okButton.setSize(75, 40);		 	    
+			  
+			    x.setLocation(25, 5);		 	   
+			    xLabel.setLocation(5, 35);
+			    y.setLocation(135, 5);
+			    yLabel.setLocation(115, 35);
+			    okButton.setLocation(65, 75);
+			    
+			    frame.add(okButton);		 	
+			    frame.add(x);
+			    frame.add(y);
+			    frame.add(yLabel);
+			    frame.add(xLabel);
+			    frame.setResizable(false);		 	
+			    frame.setVisible(true);
+			    frame.setTitle("Podaj x i y");	
+
+ 			} else if(checkWhichTest.equals("RightTrapezoid")) {
+ 			  	JDialog frame = new JDialog();
+			    frame.setLayout(null);
+			    frame.setSize(230, 170);
+			    frame.setLocation(300, 250);
+			    JTextField x = new JTextField();
+			    JLabel xLabel = new JLabel("Przesunięcie x");
+			    JTextField y = new JTextField(); 	   
+			    JLabel yLabel = new JLabel("Przesunięcie y");
+			    JButton okButton = new JButton("OK");
+			    
+			    okButton.addActionListener (new ActionListener () {
+			        public void actionPerformed(ActionEvent e) {			        			        			        	
+			        	frame.dispose();
+			        	FactoryCommand.createRightTrapezoid(-50, -50, 100, 200, Integer.parseInt(x.getText()), 	Integer.parseInt(y.getText())).execute(FeaturesManager.getDriverManager().getCurrentPlotter());
+			        }
+			    });	 	    
+			    
+			    x.setSize(50, 25);
+			    y.setSize(50, 25);
+			    xLabel.setSize(100, 25);
+			    yLabel.setSize(100, 25);
+			
+			    okButton.setSize(75, 40);		 	    
+			  
+			    x.setLocation(25, 5);		 	   
+			    xLabel.setLocation(5, 35);
+			    y.setLocation(135, 5);
+			    yLabel.setLocation(115, 35);
+			    okButton.setLocation(65, 75);
+			    
+			    frame.add(okButton);		 	
+			    frame.add(x);
+			    frame.add(y);
+			    frame.add(yLabel);
+			    frame.add(xLabel);
+			    frame.setResizable(false);		 	
+			    frame.setVisible(true);
+			    frame.setTitle("Podaj x i y");	
+ 			}	
+ 		} else {
+ 			if(checkWhichTest.equals("Rectangle")) {		
+ 				FactoryCommand.createRectangle(-50, -50, 100, 200, 	0, 0).execute(FeaturesManager.getDriverManager().getCurrentPlotter());
+ 			} else if(checkWhichTest.equals("RightTrapezoid")) {
+ 				FactoryCommand.createRightTrapezoid(-50, -50, 100, 200, 0, 0).execute(FeaturesManager.getDriverManager().getCurrentPlotter());
+ 			}
  		}
-		
-		
 		
 	}
 
