a142cf1ff8a58cde417bcca19968f158687aced7
Blazej Lewandowski <ledek89@gmail.com>
2017-06-08 02:06:59 +0200

    Poprawiony wygląd extreme points

--- src/main/java/edu/iis/powp/window/ExtremePointsState.java
+++ src/main/java/edu/iis/powp/window/ExtremePointsState.java
@@ -10,10 +10,10 @@ public class ExtremePointsState implements WindowXYState {
 		windowXY.getFrame().setSize(330, 230);
 		windowXY.getFrame().setLocation(300, 250);
 		JLabel extremePointsLabel = new JLabel("Najbardziej wysunięte punkty rysunku:");
-		JLabel plusX = new JLabel("x:");
-		JLabel plusY = new JLabel("y:");
-		JLabel minusX = new JLabel("-x:");
-		JLabel minusY = new JLabel("-y:");
+		JLabel plusX = new JLabel("max(x):");
+		JLabel plusY = new JLabel("max(y):");
+		JLabel minusX = new JLabel("min(x):");
+		JLabel minusY = new JLabel("min(y):");
 		windowXY.getX().setEditable(false);
 		windowXY.getY().setEditable(false);
 		windowXY.getLabel().setEditable(false);
@@ -21,10 +21,10 @@ public class ExtremePointsState implements WindowXYState {
 		
 		extremePointsLabel.setSize(250, 25);
 		windowXY.getOkButton().setSize(75, 40);
-		plusX.setSize(25,25);
-		plusY.setSize(25,25);
-		minusX.setSize(25,25);
-		minusY.setSize(25,25);
+		plusX.setSize(100,25);
+		plusY.setSize(100,25);
+		minusX.setSize(100,25);
+		minusY.setSize(100,25);
 		windowXY.getX().setSize(50, 25);
 		windowXY.getY().setSize(50, 25);
 		windowXY.getLabel().setSize(50, 25);
@@ -32,12 +32,12 @@ public class ExtremePointsState implements WindowXYState {
 		
 		extremePointsLabel.setLocation(35, 10);	
 		windowXY.getOkButton().setLocation(125, 155);
-		plusX.setLocation(80, 35);
-		plusY.setLocation(220, 35);
+		plusX.setLocation(60, 35);
+		plusY.setLocation(200, 35);
 		windowXY.getX().setLocation(60, 60);
 		windowXY.getY().setLocation(200, 60);
-		minusX.setLocation(80, 100);
-		minusY.setLocation(220, 100);
+		minusX.setLocation(60, 100);
+		minusY.setLocation(200, 100);
 		windowXY.getLabel().setLocation(60, 125);
 		windowXY.getSecondLabel().setLocation(200, 125);
 		
